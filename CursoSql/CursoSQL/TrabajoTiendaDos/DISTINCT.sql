3fbbd7b61d149f3afe70a376a63ada975b511adc
koulquiango <kevin.orlando14042000@gmail.com>
2021-11-02 14:13:21 -0500

    Distinct

--- CursoSql/CursoSQL/TrabajoTiendaDos/DISTINCT.sql
+++ CursoSql/CursoSQL/TrabajoTiendaDos/DISTINCT.sql
@@ -2,3 +2,7 @@ USE Facturas
 /*Retorna solo una ves el parametro a buscar a pesar que existan mas de uno con el mismo nombre*/
 select distinct nombre from Producto
 select distinct fabricacion from Producto
+
+select distinct nombre, marca 
+from Producto
+order by marca
